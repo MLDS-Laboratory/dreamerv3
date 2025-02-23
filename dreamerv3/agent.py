070ae34ffe2a3f12676d8e3f6a5e96115293fc22
aarunsrinivas5 <aarun5@umd.edu>
2025-02-23 15:08:33 -0500

    Fixed beta

--- dreamerv3/agent.py
+++ dreamerv3/agent.py
@@ -67,15 +67,14 @@ class Agent(embodied.jax.Agent):
         embodied.jax.MLPHead(scalar, **config.value, name='slowval'),
         source=self.val, **config.slowvalue)
     
-    self.beta = nj.Variable(jnp.array, 1, f32, name='beta')
+    self.beta = nj.Variable(jnp.array, -0.001, f32, name='beta')
 
     self.retnorm = embodied.jax.Normalize(**config.retnorm, name='retnorm')
     self.valnorm = embodied.jax.Normalize(**config.valnorm, name='valnorm')
     self.advnorm = embodied.jax.Normalize(**config.advnorm, name='advnorm')
-    self.srpnorm = embodied.jax.Normalize(**config.srpnorm, name='srpnorm')
 
     self.modules = [
-        self.dyn, self.enc, self.dec, self.rew, self.con, self.pol, self.val, self.beta]
+        self.dyn, self.enc, self.dec, self.rew, self.con, self.pol, self.val]
     self.opt = embodied.jax.Optimizer(
         self.modules, self._make_opt(**config.opt), summary_depth=1,
         name='opt')
@@ -143,7 +142,7 @@ class Agent(embodied.jax.Agent):
         self.loss, carry, obs, prevact, training=True, has_aux=True)
     metrics.update(mets)
     self.slowval.update()
-    self.beta.write(jnp.clip(self.beta.read(), 1e-6, 1000))
+    # self.beta.write(jnp.clip(self.beta.read(), 1e-6, 1000))
 
     outs = {}
     if self.config.replay_context:
@@ -236,7 +235,6 @@ class Agent(embodied.jax.Agent):
           self.slowval(inp, 2),
           self.beta.read(),
           self.valnorm,
-          self.srpnorm,
           update=training,
           horizon=self.config.horizon,
           **self.config.repl_loss)
@@ -420,11 +418,9 @@ def imag_loss(
   logpi = sum([v.logp(sg(act[k]))[:, :-1] for k, v in policy.items()])
   ents = {k: v.entropy()[:, :-1] for k, v in policy.items()}
   policy_loss = sg(weight[:, :-1]) * -(
-      logpi * sg(adv_normed) + actent * sg(beta) * sum(ents.values()))
-  beta_loss = sg(weight[:, :-1]) * (sg(adv_normed) * (beta ** 2) / 2 + actent * beta * sg(sum(ents.values())))
-  # beta_loss = sg(weight[:, :-1]) * (sg(adv_normed) * jnp.log(beta) + actent * beta * sg(sum(ents.values())))
+      logpi * jnp.sign(beta) * sg(adv_normed) + actent * sg(beta) * sum(ents.values()))
+
   losses['policy'] = policy_loss
-  losses['beta'] = beta_loss
 
   voffset, vscale = valnorm(ret, update)
   tar_normed = (ret - voffset) / vscale
@@ -447,7 +443,6 @@ def imag_loss(
   metrics['ret_min'] = ret_normed.min()
   metrics['ret_max'] = ret_normed.max()
   metrics['ret_rate'] = (jnp.abs(ret_normed) >= 1.0).mean()
-  metrics['beta'] = beta
   
   for k in act:
     metrics[f'ent/{k}'] = ents[k].mean()
@@ -463,7 +458,7 @@ def imag_loss(
 def repl_loss(
     last, term, rew, boot, surprise, 
     value, slowvalue, beta, 
-    valnorm, srpnorm,
+    valnorm,
     update=True,
     slowreg=1.0,
     slowtar=True,
@@ -486,18 +481,10 @@ def repl_loss(
   losses['repval'] = weight[:, :-1] * (
       value.loss(sg(ret_padded)) +
       slowreg * value.loss(sg(slowvalue.pred())))[:, :-1]
-  
-  # soffset, sscale = srpnorm(surprise, update)
-  # srp_normed = (surprise - soffset) / sscale
-  # losses['beta'] = sg(beta) * (srp_normed - 0.1)
 
   outs = {}
   outs['ret'] = ret
   metrics = {}
-  # metrics['beta'] = beta
-  # metrics['srp'] = srp_normed.mean()
-  # metrics['srp_min'] = srp_normed.min()
-  # metrics['srp_max'] = srp_normed.max()
 
   return losses, outs, metrics
 
@@ -510,3 +497,4 @@ def lambda_return(last, term, rew, val, boot, beta, disc, lam):
   for t in reversed(range(live.shape[1])):
     rets.append(jnp.exp(interm[:, t] + live[:, t] * cont[:, t] * jnp.log(1e-15 + jax.nn.relu(rets[-1]))))
   return jnp.stack(list(reversed(rets))[:-1], 1)
+
